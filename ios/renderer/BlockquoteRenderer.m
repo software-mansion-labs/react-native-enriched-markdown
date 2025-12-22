6ea6b46df0507c8b8a7d406db25f7d17d6d7ad31
Gregory Moskaliuk <mosckalyuck@gmail.com>
2025-12-22 13:57:40 +0100

    refactor(ios): enhance blockquote rendering with improved styling and spacing logic

--- ios/renderer/BlockquoteRenderer.m
+++ ios/renderer/BlockquoteRenderer.m
@@ -6,6 +6,9 @@
 #import "RendererFactory.h"
 #import "RichTextConfig.h"
 
+static NSString *const kNestedInfoDepthKey = @"depth";
+static NSString *const kNestedInfoRangeKey = @"range";
+
 @implementation BlockquoteRenderer {
   RendererFactory *_rendererFactory;
 }
@@ -35,16 +38,16 @@
               fontWeight:[config blockquoteFontWeight]
                    color:[config blockquoteColor]];
 
-  UIFont *blockquoteFont =
-      fontFromProperties([config blockquoteFontSize], [config blockquoteFontFamily], [config blockquoteFontWeight])
-          ?: font;
+  BlockStyle *blockStyle = [context getBlockStyle];
+  UIFont *blockquoteFont = fontFromBlockStyle(blockStyle) ?: font;
+  UIColor *blockquoteColor = blockStyle.color ?: color;
 
   NSUInteger blockquoteStart = output.length;
   @try {
     [_rendererFactory renderChildrenOfNode:node
                                       into:output
                                   withFont:blockquoteFont
-                                     color:[config blockquoteColor] ?: color
+                                     color:blockquoteColor
                                    context:context];
   } @finally {
     [context clearBlockStyle];
@@ -52,28 +55,41 @@
   }
 
   NSUInteger blockquoteEnd = output.length;
+  if (blockquoteEnd <= blockquoteStart) {
+    return;
+  }
+
+  [self applyStylingAndSpacing:output
+                        config:config
+               blockquoteStart:blockquoteStart
+                 blockquoteEnd:blockquoteEnd
+                  currentDepth:currentDepth];
+}
+
+#pragma mark - Styling and Spacing
+
+- (void)applyStylingAndSpacing:(NSMutableAttributedString *)output
+                        config:(RichTextConfig *)config
+               blockquoteStart:(NSUInteger)blockquoteStart
+                 blockquoteEnd:(NSUInteger)blockquoteEnd
+                  currentDepth:(NSInteger)currentDepth
+{
   NSRange blockquoteRange = NSMakeRange(blockquoteStart, blockquoteEnd - blockquoteStart);
   CGFloat levelSpacing = [config blockquoteBorderWidth] + [config blockquoteGapWidth];
+  NSArray<NSDictionary *> *nestedInfo = [self collectNestedBlockquotes:output range:blockquoteRange depth:currentDepth];
 
-  // Collect nested blockquotes
-  NSMutableArray<NSDictionary *> *nestedInfo = [self collectNestedBlockquotes:output
-                                                                        range:blockquoteRange
-                                                                        depth:currentDepth];
-
-  // Apply base blockquote styling
-  [self applyBlockquoteStyle:output
-                       range:blockquoteRange
-                       depth:currentDepth
-                levelSpacing:levelSpacing
-             backgroundColor:[config blockquoteBackgroundColor]
-                  lineHeight:[config blockquoteLineHeight]];
+  [self applyBaseBlockquoteStyle:output
+                           range:blockquoteRange
+                           depth:currentDepth
+                    levelSpacing:levelSpacing
+                 backgroundColor:[config blockquoteBackgroundColor]
+                      lineHeight:[config blockquoteLineHeight]];
 
   // Re-apply nested blockquote styles to preserve their indentation
   [self reapplyNestedStyles:output nestedInfo:nestedInfo levelSpacing:levelSpacing];
 
-  // Add nestedMarginBottom spacing between nested blockquotes
   CGFloat nestedSpacing = [config blockquoteNestedMarginBottom];
-  if (nestedSpacing > 0) {
+  if (nestedSpacing > 0 && nestedInfo.count > 0) {
     [self applyNestedSpacing:output
                   nestedInfo:nestedInfo
                        depth:currentDepth
@@ -83,42 +99,48 @@
              backgroundColor:[config blockquoteBackgroundColor]];
   }
 
-  // Apply marginBottom after the entire blockquote ends (only for top-level blockquotes)
-  CGFloat marginBottom = [config blockquoteMarginBottom];
-  if (marginBottom > 0 && blockquoteEnd > blockquoteStart && currentDepth == 0) {
-    applyBlockquoteSpacing(output, marginBottom);
+  // Apply marginBottom for top-level blockquotes only
+  if (currentDepth == 0) {
+    CGFloat marginBottom = [config blockquoteMarginBottom];
+    if (marginBottom > 0) {
+      applyBlockquoteSpacing(output, marginBottom);
+    }
   }
 }
 
-#pragma mark - Helper Methods
+#pragma mark - Nested Blockquote Handling
 
-- (NSMutableArray<NSDictionary *> *)collectNestedBlockquotes:(NSMutableAttributedString *)output
-                                                       range:(NSRange)blockquoteRange
-                                                       depth:(NSInteger)currentDepth
+- (NSArray<NSDictionary *> *)collectNestedBlockquotes:(NSMutableAttributedString *)output
+                                                range:(NSRange)blockquoteRange
+                                                depth:(NSInteger)currentDepth
 {
   NSMutableArray<NSDictionary *> *nestedInfo = [NSMutableArray array];
-  [output enumerateAttribute:RichTextBlockquoteDepthAttributeName
-                     inRange:blockquoteRange
-                     options:NSAttributedStringEnumerationLongestEffectiveRangeNotRequired
-                  usingBlock:^(id value, NSRange range, BOOL *stop) {
-                    if (value && [value integerValue] > currentDepth) {
-                      [nestedInfo addObject:@{@"depth" : value, @"range" : [NSValue valueWithRange:range]}];
-                    }
-                  }];
+  [output
+      enumerateAttribute:RichTextBlockquoteDepthAttributeName
+                 inRange:blockquoteRange
+                 options:NSAttributedStringEnumerationLongestEffectiveRangeNotRequired
+              usingBlock:^(id value, NSRange range, BOOL *stop) {
+                NSInteger depth = [value integerValue];
+                if (value && depth > currentDepth) {
+                  [nestedInfo
+                      addObject:@{kNestedInfoDepthKey : value, kNestedInfoRangeKey : [NSValue valueWithRange:range]}];
+                }
+              }];
   return nestedInfo;
 }
 
-- (void)applyBlockquoteStyle:(NSMutableAttributedString *)output
-                       range:(NSRange)blockquoteRange
-                       depth:(NSInteger)currentDepth
-                levelSpacing:(CGFloat)levelSpacing
-             backgroundColor:(UIColor *)backgroundColor
-                  lineHeight:(CGFloat)lineHeight
+- (void)applyBaseBlockquoteStyle:(NSMutableAttributedString *)output
+                           range:(NSRange)blockquoteRange
+                           depth:(NSInteger)currentDepth
+                    levelSpacing:(CGFloat)levelSpacing
+                 backgroundColor:(UIColor *)backgroundColor
+                      lineHeight:(CGFloat)lineHeight
 {
   NSMutableParagraphStyle *paragraphStyle = getOrCreateParagraphStyle(output, blockquoteRange.location);
-  CGFloat totalIndent = (currentDepth + 1) * levelSpacing;
+  CGFloat totalIndent = [self calculateIndentForDepth:currentDepth levelSpacing:levelSpacing];
   paragraphStyle.firstLineHeadIndent = totalIndent;
   paragraphStyle.headIndent = totalIndent;
+
   [output addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:blockquoteRange];
   [output addAttribute:RichTextBlockquoteDepthAttributeName value:@(currentDepth) range:blockquoteRange];
 
@@ -130,21 +152,25 @@
 }
 
 - (void)reapplyNestedStyles:(NSMutableAttributedString *)output
-                 nestedInfo:(NSMutableArray<NSDictionary *> *)nestedInfo
+                 nestedInfo:(NSArray<NSDictionary *> *)nestedInfo
                levelSpacing:(CGFloat)levelSpacing
 {
   for (NSDictionary *info in nestedInfo) {
-    NSRange nestedRange = [info[@"range"] rangeValue];
-    NSInteger nestedDepth = [info[@"depth"] integerValue];
+    NSRange nestedRange = [info[kNestedInfoRangeKey] rangeValue];
+    NSInteger nestedDepth = [info[kNestedInfoDepthKey] integerValue];
     NSMutableParagraphStyle *style = getOrCreateParagraphStyle(output, nestedRange.location);
-    CGFloat indent = (nestedDepth + 1) * levelSpacing;
+
+    CGFloat indent = [self calculateIndentForDepth:nestedDepth levelSpacing:levelSpacing];
     style.firstLineHeadIndent = indent;
     style.headIndent = indent;
     style.tailIndent = 0;
+
     [output addAttribute:NSParagraphStyleAttributeName value:style range:nestedRange];
-    [output addAttribute:RichTextBlockquoteDepthAttributeName value:info[@"depth"] range:nestedRange];
+    [output addAttribute:RichTextBlockquoteDepthAttributeName value:info[kNestedInfoDepthKey] range:nestedRange];
   }
 }
+
+#pragma mark - Spacer Utilities
 
 - (NSMutableParagraphStyle *)createSpacerStyle:(CGFloat)spacing
 {
@@ -163,8 +189,12 @@
 
 - (BOOL)hasNewlineAtLocation:(NSUInteger)location inString:(NSMutableAttributedString *)output
 {
-  return (location < output.length && [[[output attributedSubstringFromRange:NSMakeRange(location, 1)] string]
-                                          isEqualToString:@"\n"]);
+  if (location >= output.length) {
+    return NO;
+  }
+  NSRange charRange = NSMakeRange(location, 1);
+  NSString *charAtLocation = [[output attributedSubstringFromRange:charRange] string];
+  return [charAtLocation isEqualToString:@"\n"];
 }
 
 - (BOOL)insertSpacerAtLocation:(NSUInteger)location
@@ -179,28 +209,27 @@
     insertedNewline = YES;
   }
 
-  [output addAttribute:NSParagraphStyleAttributeName
-                 value:[self createSpacerStyle:spacing]
-                 range:NSMakeRange(location, 1)];
-  [output addAttribute:RichTextBlockquoteDepthAttributeName value:depth range:NSMakeRange(location, 1)];
-  [output removeAttribute:NSFontAttributeName range:NSMakeRange(location, 1)];
+  NSRange spacerRange = NSMakeRange(location, 1);
+  [output addAttribute:NSParagraphStyleAttributeName value:[self createSpacerStyle:spacing] range:spacerRange];
+  [output addAttribute:RichTextBlockquoteDepthAttributeName value:depth range:spacerRange];
+  [output removeAttribute:NSFontAttributeName range:spacerRange];
 
   if (backgroundColor) {
-    [output addAttribute:RichTextBlockquoteBackgroundColorAttributeName
-                   value:backgroundColor
-                   range:NSMakeRange(location, 1)];
+    [output addAttribute:RichTextBlockquoteBackgroundColorAttributeName value:backgroundColor range:spacerRange];
   }
 
   return insertedNewline;
 }
 
+#pragma mark - Range Utilities
+
 - (void)updateRangesAfterInsertion:(NSUInteger)insertionPoint ranges:(NSMutableArray<NSDictionary *> *)ranges
 {
   for (NSInteger idx = 0; idx < ranges.count; idx++) {
-    NSRange range = [ranges[idx][@"range"] rangeValue];
+    NSRange range = [ranges[idx][kNestedInfoRangeKey] rangeValue];
     if (range.location >= insertionPoint) {
       NSMutableDictionary *updated = [ranges[idx] mutableCopy];
-      updated[@"range"] = [NSValue valueWithRange:NSMakeRange(range.location + 1, range.length)];
+      updated[kNestedInfoRangeKey] = [NSValue valueWithRange:NSMakeRange(range.location + 1, range.length)];
       ranges[idx] = updated;
     }
   }
@@ -209,84 +238,123 @@
 - (NSArray<NSDictionary *> *)sortByLocation:(NSArray<NSDictionary *> *)items
 {
   return [items sortedArrayUsingComparator:^NSComparisonResult(NSDictionary *obj1, NSDictionary *obj2) {
-    NSRange r1 = [obj1[@"range"] rangeValue], r2 = [obj2[@"range"] rangeValue];
-    return r1.location < r2.location ? NSOrderedAscending
-                                     : (r1.location > r2.location ? NSOrderedDescending : NSOrderedSame);
+    NSRange r1 = [obj1[kNestedInfoRangeKey] rangeValue];
+    NSRange r2 = [obj2[kNestedInfoRangeKey] rangeValue];
+    if (r1.location < r2.location) {
+      return NSOrderedAscending;
+    } else if (r1.location > r2.location) {
+      return NSOrderedDescending;
+    }
+    return NSOrderedSame;
   }];
 }
 
-- (NSMutableDictionary<NSNumber *, NSMutableArray<NSDictionary *> *> *)groupByDepth:
-    (NSArray<NSDictionary *> *)nestedInfo
+- (NSDictionary<NSNumber *, NSArray<NSDictionary *> *> *)groupByDepth:(NSArray<NSDictionary *> *)nestedInfo
 {
   NSMutableDictionary<NSNumber *, NSMutableArray<NSDictionary *> *> *byDepth = [NSMutableDictionary dictionary];
   for (NSDictionary *info in nestedInfo) {
-    NSNumber *depth = info[@"depth"];
-    if (!byDepth[depth]) {
-      byDepth[depth] = [NSMutableArray array];
+    NSNumber *depth = info[kNestedInfoDepthKey];
+    NSMutableArray<NSDictionary *> *group = byDepth[depth];
+    if (!group) {
+      group = [NSMutableArray array];
+      byDepth[depth] = group;
     }
-    [byDepth[depth] addObject:info];
+    [group addObject:info];
   }
   return byDepth;
 }
 
 - (void)applyNestedSpacing:(NSMutableAttributedString *)output
-                nestedInfo:(NSMutableArray<NSDictionary *> *)nestedInfo
+                nestedInfo:(NSArray<NSDictionary *> *)nestedInfo
                      depth:(NSInteger)currentDepth
                    spacing:(CGFloat)spacing
            blockquoteStart:(NSUInteger)blockquoteStart
              blockquoteEnd:(NSUInteger)blockquoteEnd
            backgroundColor:(UIColor *)backgroundColor
 {
-  // Add spacing between parent content and first nested blockquote
-  if (nestedInfo.count > 0) {
-    NSArray<NSDictionary *> *sorted = [self sortByLocation:nestedInfo];
-    NSRange firstRange = [sorted[0][@"range"] rangeValue];
-
-    if (firstRange.location > blockquoteStart) {
-      [self insertSpacerAtLocation:firstRange.location
-                             depth:@(currentDepth)
-                           spacing:spacing
-                   backgroundColor:backgroundColor
-                            output:output];
-      [self updateRangesAfterInsertion:firstRange.location ranges:nestedInfo];
-    }
+  if (nestedInfo.count == 0) {
+    return;
   }
 
-  // Add spacing between consecutive nested blockquotes at the same depth
-  if (nestedInfo.count > 1) {
-    NSMutableDictionary<NSNumber *, NSMutableArray<NSDictionary *> *> *byDepth = [self groupByDepth:nestedInfo];
+  NSArray<NSDictionary *> *sorted = [self sortByLocation:nestedInfo];
+  NSMutableArray<NSDictionary *> *mutableNestedInfo = [nestedInfo mutableCopy];
 
-    for (NSNumber *depthKey in byDepth.allKeys) {
-      NSMutableArray<NSDictionary *> *group = [self sortByLocation:byDepth[depthKey]].mutableCopy;
-      if (group.count < 2)
+  NSRange firstRange = [sorted[0][kNestedInfoRangeKey] rangeValue];
+  if (firstRange.location > blockquoteStart) {
+    [self insertSpacerAtLocation:firstRange.location
+                           depth:@(currentDepth)
+                         spacing:spacing
+                 backgroundColor:backgroundColor
+                          output:output];
+    [self updateRangesAfterInsertion:firstRange.location ranges:mutableNestedInfo];
+  }
+
+  if (sorted.count > 1) {
+    [self applySpacingBetweenConsecutiveBlockquotes:output
+                                         sortedInfo:sorted
+                                            byDepth:[self groupByDepth:mutableNestedInfo]
+                                            spacing:spacing
+                                    backgroundColor:backgroundColor
+                                      blockquoteEnd:blockquoteEnd
+                                         nestedInfo:mutableNestedInfo];
+  }
+}
+
+- (void)applySpacingBetweenConsecutiveBlockquotes:(NSMutableAttributedString *)output
+                                       sortedInfo:(NSArray<NSDictionary *> *)sortedInfo
+                                          byDepth:(NSDictionary<NSNumber *, NSArray<NSDictionary *> *> *)byDepth
+                                          spacing:(CGFloat)spacing
+                                  backgroundColor:(UIColor *)backgroundColor
+                                    blockquoteEnd:(NSUInteger)blockquoteEnd
+                                       nestedInfo:(NSMutableArray<NSDictionary *> *)nestedInfo
+{
+  for (NSNumber *depthKey in byDepth.allKeys) {
+    NSArray<NSDictionary *> *group = [self sortByLocation:byDepth[depthKey]];
+    if (group.count < 2) {
+      continue;
+    }
+
+    NSUInteger adjustedBlockquoteEnd = blockquoteEnd;
+    for (NSInteger i = group.count - 2; i >= 0; i--) {
+      NSRange currentRange = [group[i][kNestedInfoRangeKey] rangeValue];
+      NSRange nextRange = [group[i + 1][kNestedInfoRangeKey] rangeValue];
+      NSUInteger currentEnd = NSMaxRange(currentRange);
+      NSUInteger nextEnd = NSMaxRange(nextRange);
+
+      // Skip if next is last content or not consecutive
+      if ([self shouldSkipSpacingInsertion:nextEnd
+                               adjustedEnd:adjustedBlockquoteEnd
+                                currentEnd:currentEnd
+                              nextLocation:nextRange.location]) {
         continue;
+      }
 
-      NSUInteger adjustedBlockquoteEnd = blockquoteEnd;
-      for (NSInteger i = group.count - 2; i >= 0; i--) {
-        NSRange currentRange = [group[i][@"range"] rangeValue];
-        NSRange nextRange = [group[i + 1][@"range"] rangeValue];
-        NSUInteger currentEnd = NSMaxRange(currentRange);
-        NSUInteger nextEnd = NSMaxRange(nextRange);
-
-        // Skip if next is last content or not consecutive
-        if (nextEnd >= adjustedBlockquoteEnd - 1)
-          continue;
-        if (nextRange.location < currentEnd || nextRange.location > currentEnd + 1)
-          continue;
-
-        BOOL inserted = [self insertSpacerAtLocation:currentEnd
-                                               depth:depthKey
-                                             spacing:spacing
-                                     backgroundColor:backgroundColor
-                                              output:output];
-        if (inserted) {
-          adjustedBlockquoteEnd++;
-          [self updateRangesAfterInsertion:currentEnd ranges:nestedInfo];
-          [self updateRangesAfterInsertion:currentEnd ranges:group];
-        }
+      BOOL inserted = [self insertSpacerAtLocation:currentEnd
+                                             depth:depthKey
+                                           spacing:spacing
+                                   backgroundColor:backgroundColor
+                                            output:output];
+      if (inserted) {
+        adjustedBlockquoteEnd++;
+        [self updateRangesAfterInsertion:currentEnd ranges:nestedInfo];
       }
     }
   }
+}
+
+#pragma mark - Helper Methods
+
+- (CGFloat)calculateIndentForDepth:(NSInteger)depth levelSpacing:(CGFloat)levelSpacing
+{
+  return (depth + 1) * levelSpacing;
+}
+
+- (BOOL)shouldSkipSpacingInsertion:(NSUInteger)nextEnd
+                       adjustedEnd:(NSUInteger)adjustedEnd
+                        currentEnd:(NSUInteger)currentEnd
+                      nextLocation:(NSUInteger)nextLocation
+{
+  return (nextEnd >= adjustedEnd - 1) || (nextLocation < currentEnd) || (nextLocation > currentEnd + 1);
 }
 
 @end

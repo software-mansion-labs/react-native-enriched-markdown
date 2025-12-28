d92a5bd96d25c6c62eaf342064d077a519e83189
Gregory Moskaliuk <mosckalyuck@gmail.com>
2025-12-28 13:26:58 +0100

    refactor(ios): enhance BlockquoteBorder to support spacer detection

--- ios/utils/BlockquoteBorder.m
+++ ios/utils/BlockquoteBorder.m
@@ -7,6 +7,7 @@ NSString *const RichTextBlockquoteBackgroundColorAttributeName = @"RichTextBlock
 static NSString *const kFragmentRectKey = @"rect";
 static NSString *const kFragmentDepthKey = @"depth";
 static NSString *const kFragmentDepthLocationKey = @"depthLocation";
+static NSString *const kFragmentIsSpacerKey = @"isSpacer";
 
 @implementation BlockquoteBorder {
   RichTextConfig *_config;
@@ -32,61 +33,90 @@ static NSString *const kFragmentDepthLocationKey = @"depthLocation";
   UIColor *borderColor = [_config blockquoteBorderColor];
   CGFloat borderWidth = [_config blockquoteBorderWidth];
   CGFloat levelSpacing = borderWidth + [_config blockquoteGapWidth];
+  CGFloat nestedMarginBottom = [_config blockquoteNestedMarginBottom];
   CGFloat containerWidth = textContainer.size.width;
 
   NSMutableArray<NSDictionary *> *fragments = [NSMutableArray array];
+  NSMutableDictionary<NSNumber *, NSNumber *> *firstCharIndexForDepth = [NSMutableDictionary dictionary];
 
-  [layoutManager enumerateLineFragmentsForGlyphRange:glyphsToShow
-                                          usingBlock:^(CGRect rect, CGRect usedRect, NSTextContainer *container,
-                                                       NSRange glyphRange, BOOL *stop) {
-                                            NSRange charRange = [layoutManager characterRangeForGlyphRange:glyphRange
-                                                                                          actualGlyphRange:NULL];
-                                            if (charRange.location == NSNotFound || charRange.length == 0) {
-                                              return;
-                                            }
+  [layoutManager
+      enumerateLineFragmentsForGlyphRange:glyphsToShow
+                               usingBlock:^(CGRect rect, CGRect usedRect, NSTextContainer *container,
+                                            NSRange glyphRange, BOOL *stop) {
+                                 NSRange charRange = [layoutManager characterRangeForGlyphRange:glyphRange
+                                                                               actualGlyphRange:NULL];
+                                 if (charRange.location == NSNotFound || charRange.length == 0) {
+                                   return;
+                                 }
 
-                                            NSNumber *depth =
-                                                [textStorage attribute:RichTextBlockquoteDepthAttributeName
-                                                               atIndex:charRange.location
-                                                        effectiveRange:NULL];
-                                            if (!depth) {
-                                              return;
-                                            }
+                                 NSNumber *depth = [textStorage attribute:RichTextBlockquoteDepthAttributeName
+                                                                  atIndex:charRange.location
+                                                           effectiveRange:NULL];
+                                 if (!depth) {
+                                   return;
+                                 }
 
-                                            [fragments addObject:@{
-                                              kFragmentRectKey : [NSValue valueWithCGRect:rect],
-                                              kFragmentDepthKey : depth,
-                                              kFragmentDepthLocationKey : @(charRange.location)
-                                            }];
-                                          }];
+                                 NSUInteger charLocation = charRange.location;
+                                 BOOL isSpacer = nestedMarginBottom > 0
+                                                     ? [self isSpacerAtLocation:charLocation textStorage:textStorage]
+                                                     : NO;
+
+                                 if (nestedMarginBottom > 0 && !firstCharIndexForDepth[depth] && !isSpacer) {
+                                   firstCharIndexForDepth[depth] = @(charLocation);
+                                 }
+
+                                 [fragments addObject:@{
+                                   kFragmentRectKey : [NSValue valueWithCGRect:rect],
+                                   @"usedRect" : [NSValue valueWithCGRect:usedRect],
+                                   kFragmentDepthKey : depth,
+                                   kFragmentDepthLocationKey : @(charLocation),
+                                   kFragmentIsSpacerKey : @(isSpacer)
+                                 }];
+                               }];
 
   for (NSDictionary *fragment in fragments) {
     [self drawFragment:fragment
-           textStorage:textStorage
-                origin:origin
-          levelSpacing:levelSpacing
-           borderColor:borderColor
-           borderWidth:borderWidth
-        containerWidth:containerWidth];
+                   textStorage:textStorage
+                        origin:origin
+                  levelSpacing:levelSpacing
+            nestedMarginBottom:nestedMarginBottom
+        firstCharIndexForDepth:firstCharIndexForDepth
+                   borderColor:borderColor
+                   borderWidth:borderWidth
+                containerWidth:containerWidth];
   }
 }
 
-#pragma mark - Drawing
+#pragma mark - Helper Methods
+
+- (BOOL)isSpacerAtLocation:(NSUInteger)location textStorage:(NSTextStorage *)textStorage
+{
+  NSParagraphStyle *paraStyle = [textStorage attribute:NSParagraphStyleAttributeName
+                                               atIndex:location
+                                        effectiveRange:NULL];
+  if (!paraStyle) {
+    return NO;
+  }
+  return (paraStyle.headIndent == 0 && paraStyle.minimumLineHeight > 0 &&
+          fabs(paraStyle.minimumLineHeight - paraStyle.maximumLineHeight) < 0.001);
+}
 
 - (void)drawFragment:(NSDictionary *)fragment
-         textStorage:(NSTextStorage *)textStorage
-              origin:(CGPoint)origin
-        levelSpacing:(CGFloat)levelSpacing
-         borderColor:(UIColor *)borderColor
-         borderWidth:(CGFloat)borderWidth
-      containerWidth:(CGFloat)containerWidth
+               textStorage:(NSTextStorage *)textStorage
+                    origin:(CGPoint)origin
+              levelSpacing:(CGFloat)levelSpacing
+        nestedMarginBottom:(CGFloat)nestedMarginBottom
+    firstCharIndexForDepth:(NSDictionary<NSNumber *, NSNumber *> *)firstCharIndexForDepth
+               borderColor:(UIColor *)borderColor
+               borderWidth:(CGFloat)borderWidth
+            containerWidth:(CGFloat)containerWidth
 {
   CGRect rect = [fragment[kFragmentRectKey] CGRectValue];
   NSInteger depth = [fragment[kFragmentDepthKey] integerValue];
   NSUInteger charLocation = [fragment[kFragmentDepthLocationKey] unsignedIntegerValue];
+  BOOL isSpacer = [fragment[kFragmentIsSpacerKey] boolValue];
   CGFloat baseY = origin.y + rect.origin.y;
 
-  // Draw background if configured
   UIColor *backgroundColor = [textStorage attribute:RichTextBlockquoteBackgroundColorAttributeName
                                             atIndex:charLocation
                                      effectiveRange:NULL]
@@ -97,11 +127,11 @@ static NSString *const kFragmentDepthLocationKey = @"depthLocation";
     UIRectFill(bgRect);
   }
 
-  // Draw borders for each nesting level (0 to depth)
-  // Each level adds another vertical line at increasing indentation
+  // Border aligns with text content naturally since paragraphSpacing adds space after paragraphs
   for (NSInteger level = 0; level <= depth; level++) {
-    CGFloat borderX = origin.x + (levelSpacing * level);
-    CGRect borderRect = CGRectMake(borderX, baseY, borderWidth, rect.size.height);
+    CGFloat borderY = baseY;
+    CGFloat borderHeight = rect.size.height;
+    CGRect borderRect = CGRectMake(origin.x + (levelSpacing * level), borderY, borderWidth, borderHeight);
     [borderColor setFill];
     UIRectFill(borderRect);
   }

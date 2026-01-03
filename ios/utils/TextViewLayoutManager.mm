e26804efb0bbebf776d0362bbf54327c665aa1d1
Gregory Moskaliuk <mosckalyuck@gmail.com>
2026-01-03 15:55:52 +0100

    refactor(ios): enhance TextViewLayoutManager by implementing safe property accessors

--- ios/utils/TextViewLayoutManager.mm
+++ ios/utils/TextViewLayoutManager.mm
@@ -10,51 +10,72 @@
 
 - (void)drawBackgroundForGlyphRange:(NSRange)glyphsToShow atPoint:(CGPoint)origin
 {
+  // 1. UIKit standard background drawing
   [super drawBackgroundForGlyphRange:glyphsToShow atPoint:origin];
 
-  NSTextStorage *textStorage = self.textStorage;
-  if (!textStorage || textStorage.length == 0)
+  NSTextStorage *storage = self.textStorage;
+  if (!storage || storage.length == 0)
     return;
 
-  // Get text container for the glyph range
-  NSRange effectiveRange;
-  NSTextContainer *textContainer = [self textContainerForGlyphAtIndex:glyphsToShow.location
-                                                       effectiveRange:&effectiveRange];
+  // 2. Safely get the container and config
+  NSTextContainer *textContainer = [self textContainerForGlyphAtIndex:glyphsToShow.location effectiveRange:NULL];
   if (!textContainer)
     return;
 
   StyleConfig *config = self.config;
+  if (!config)
+    return;
 
-  CodeBackground *codeBackground =
-      [self getOrCreateAssociatedObject:kCodeBackgroundKey
-                                factory:^id { return [[CodeBackground alloc] initWithConfig:config]; }];
-  [codeBackground drawBackgroundsForGlyphRange:glyphsToShow
-                                 layoutManager:self
-                                 textContainer:textContainer
-                                       atPoint:origin];
+  // 3. Draw specialized layers
+  // We fetch the objects into local variables FIRST to ensure they don't
+  // disappear mid-method if setConfig: is called on another thread.
 
-  BlockquoteBorder *blockquoteBorder =
-      [self getOrCreateAssociatedObject:kBlockquoteBorderKey
-                                factory:^id { return [[BlockquoteBorder alloc] initWithConfig:config]; }];
-  [blockquoteBorder drawBordersForGlyphRange:glyphsToShow
-                               layoutManager:self
-                               textContainer:textContainer
-                                     atPoint:origin];
+  CodeBackground *codeBg = [self getCodeBackgroundWithConfig:config];
+  [codeBg drawBackgroundsForGlyphRange:glyphsToShow layoutManager:self textContainer:textContainer atPoint:origin];
 
-  ListMarkerDrawer *listMarkerDrawer =
-      [self getOrCreateAssociatedObject:kListMarkerDrawerKey
-                                factory:^id { return [[ListMarkerDrawer alloc] initWithConfig:config]; }];
-  [listMarkerDrawer drawMarkersForGlyphRange:glyphsToShow
-                               layoutManager:self
-                               textContainer:textContainer
-                                     atPoint:origin];
+  BlockquoteBorder *quoteBorder = [self getBlockquoteBorderWithConfig:config];
+  [quoteBorder drawBordersForGlyphRange:glyphsToShow layoutManager:self textContainer:textContainer atPoint:origin];
 
-  // Add other element drawing here:
-  // [self drawBlockquoteBackgroundsForGlyphRange:glyphsToShow
-  //                                 textContainer:textContainer
-  //                                        atPoint:origin
-  //                                          config:config];
+  ListMarkerDrawer *markerDrawer = [self getListMarkerDrawerWithConfig:config];
+  [markerDrawer drawMarkersForGlyphRange:glyphsToShow layoutManager:self textContainer:textContainer atPoint:origin];
 }
+
+#pragma mark - Safe Property Accessors
+
+// We split these into explicit methods to avoid the 'code 257' pointer corruption
+// that can happen with generic 'id' factory blocks.
+
+- (CodeBackground *)getCodeBackgroundWithConfig:(StyleConfig *)config
+{
+  CodeBackground *obj = objc_getAssociatedObject(self, kCodeBackgroundKey);
+  if (!obj) {
+    obj = [[CodeBackground alloc] initWithConfig:config];
+    objc_setAssociatedObject(self, kCodeBackgroundKey, obj, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+  }
+  return obj;
+}
+
+- (BlockquoteBorder *)getBlockquoteBorderWithConfig:(StyleConfig *)config
+{
+  BlockquoteBorder *obj = objc_getAssociatedObject(self, kBlockquoteBorderKey);
+  if (!obj) {
+    obj = [[BlockquoteBorder alloc] initWithConfig:config];
+    objc_setAssociatedObject(self, kBlockquoteBorderKey, obj, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+  }
+  return obj;
+}
+
+- (ListMarkerDrawer *)getListMarkerDrawerWithConfig:(StyleConfig *)config
+{
+  ListMarkerDrawer *obj = objc_getAssociatedObject(self, kListMarkerDrawerKey);
+  if (!obj) {
+    obj = [[ListMarkerDrawer alloc] initWithConfig:config];
+    objc_setAssociatedObject(self, kListMarkerDrawerKey, obj, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+  }
+  return obj;
+}
+
+#pragma mark - Configuration
 
 - (StyleConfig *)config
 {
@@ -63,24 +84,15 @@
 
 - (void)setConfig:(StyleConfig *)config
 {
-  objc_setAssociatedObject(self, kStyleConfigKey, config, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-  // Reset all drawing objects when config changes (they'll be recreated on next draw)
+  // We use the same key but clear dependencies first to ensure no stale pointers exist
   objc_setAssociatedObject(self, kCodeBackgroundKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
   objc_setAssociatedObject(self, kBlockquoteBorderKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
   objc_setAssociatedObject(self, kListMarkerDrawerKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-  // Add more resets here for other element types
-}
 
-#pragma mark - Helper Methods
+  objc_setAssociatedObject(self, kStyleConfigKey, config, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 
-- (id)getOrCreateAssociatedObject:(void *)key factory:(id (^)(void))factory
-{
-  id object = objc_getAssociatedObject(self, key);
-  if (!object) {
-    object = factory();
-    objc_setAssociatedObject(self, key, object, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-  }
-  return object;
+  // Force a full redraw
+  [self invalidateDisplayForCharacterRange:NSMakeRange(0, self.textStorage.length)];
 }
 
 @end

927737c08fccec1f8be5b0fc7b736d0221fa75a1
Gregory Moskaliuk <mosckalyuck@gmail.com>
2026-02-21 13:33:12 +0100

    feat: add nightly builds workflow with OIDC support (#98)

---
+++ scripts/release/validate-latest-version.js
@@ -0,0 +1,46 @@
+const { getPackageVersionByTag } = require('./npm-utils');
+const { parseVersion } = require('./version-utils');
+
+function validateLatestVersion(version) {
+  const [newMajor, newMinor, newPatch, newPreRelease] = parseVersion(version);
+  const latestVersion = getPackageVersionByTag(
+    'react-native-enriched-markdown',
+    'latest'
+  );
+  const [major, minor, patch] = parseVersion(latestVersion);
+
+  if (newPreRelease !== null) {
+    throw new Error(
+      `Pre-release version ${version} cannot be the latest version`
+    );
+  }
+
+  if (newMajor < major) {
+    throw new Error(
+      `New major version ${newMajor} is less than latest major version ${major}`
+    );
+  }
+
+  // For enriched markdown, we allow major version bumps
+  const isValid =
+    (newMajor === major && newMinor === minor && newPatch === patch + 1) ||
+    (newMajor === major && newMinor === minor + 1 && newPatch === 0) ||
+    (newMajor === major + 1 && newMinor === 0 && newPatch === 0);
+
+  if (!isValid) {
+    throw new Error(
+      `Version ${version} is not a valid latest version based on latest published version ${latestVersion}`
+    );
+  }
+
+  return true;
+}
+
+if (require.main === module) {
+  const version = process.argv[2];
+  console.log(validateLatestVersion(version));
+}
+
+module.exports = {
+  validateLatestVersion,
+};
